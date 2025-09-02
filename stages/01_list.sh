faba7d86f9f62892cd2efdf7df4069b1863dacf4
ajay-nagyal <anagyal@insilica.co>
2025-09-02 11:34:21 -0400

    feat(list): use a progress bar and non-verbose output

--- stages/01_list.sh
+++ stages/01_list.sh
@@ -17,12 +17,11 @@ repos=$(gh repo list "$OWNER" --visibility=public --limit "1000" --json name,own
 # For each repo, check if root-level dvc.lock exists using the contents API
 export TMPFILE
 # shellcheck disable=SC2016
-echo "$repos" | parallel '
+echo "$repos" | parallel --bar '
     repo="{}"
     if [ -n "$repo" ]; then
         if gh api -X GET -H "Accept: application/vnd.github.v3+json" "repos/$repo/contents/dvc.lock" >/dev/null 2>&1; then
             REPO_NAME=$(echo "$repo" | sed "s|^.*/||g")
-            echo "Found public repository: $REPO_NAME"
             echo $REPO_NAME >> list/bricks.txt
         fi
     fi
